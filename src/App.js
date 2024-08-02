a13b6c5ac62e058059b16b4c9410010464114bea
Ajitesh Sarangi <ajiteshsarangi2002@gmail.com>
2024-08-02 19:48:29 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Blog from "./components/LightBlogA";
+import Header from "./components/LightHeaderB";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Blog />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/LightHeaderB.js
@@ -0,0 +1,73 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function LightHeaderB(props) {
+  return (
+    <header className="text-gray-600 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a href className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className={`w-10 h-10 text-white p-2 bg-${props.theme}-500 rounded-full`}
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
+          </svg>
+          <span className="ml-3 text-xl">SpaceX</span>
+        </a>
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+          {/* ========================= */}
+          <div class="flex justify-center">
+            <div class="mb-3 xl:w-96">
+              <input
+                type="text"
+                class="
+        form-control
+        block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+      "
+                id="exampleFormControlInput1"
+                placeholder="Search"
+              />
+            </div>
+          </div>
+          {/*===============================  */}
+
+          {/* <a href className="mr-5 hover:text-gray-900">
+            Fourth Link
+          </a> */}
+        </nav>
+        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+          Filter
+        </button>
+      </div>
+    </header>
+  );
+}
+
+LightHeaderB.defaultProps = {
+  theme: "indigo",
+};
+
+LightHeaderB.propTypes = {
+  theme: PropTypes.string.isRequired,
+};
+
+export default LightHeaderB;
